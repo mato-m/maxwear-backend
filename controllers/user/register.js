9a65266c183d1a8e9bab9890d8458b195c9e4654
Mato Martinović <matom@Matos-MacBook-Pro.local>
2022-11-12 19:55:21 +0100

    Added user registration controller

--- controllers/user/register.js
+++ controllers/user/register.js
@@ -1,13 +1,33 @@
 const connection = require("../../dbConfig");
 const uuid = require("uuid");
-const register = async (req, res) => {
-  try {
-    await connection`insert into maxwear_schema.category (category_id,category_name) values (${uuid.v4()},${
-      req.body.category_name
-    })`;
-    res.send({ status: 0, message: "Successfully created a new account" });
-  } catch (error) {
-    res.send({ status: 1, message: "Error while creating a new account" });
-  }
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const register = (req, res) => {
+  const userID = uuid.v4();
+  bcrypt.hash(req.body.pass, 13, async (err, hash) => {
+    if (err) {
+      res.send({ status: 1, message: "Error while hashing password" });
+      return;
+    } else
+      try {
+        await connection`insert into maxwear_schema.appuser (appuser_id,appuser_fname,appuser_lname,appuser_mail,appuser_hash) values (${userID},${req.body.fname},${req.body.lname},${req.body.mail},${hash})`;
+        const token = jwt.sign(
+          {
+            userID,
+            fname: req.body.fname,
+            lname: req.body.lname,
+            mail: req.body.mail,
+          },
+          process.env.JWT_SECRET
+        );
+        res.send({
+          status: 0,
+          message: "Successfully created a new account",
+          token,
+        });
+      } catch (error) {
+        res.send({ status: 1, message: "Error while creating a new account" });
+      }
+  });
 };
 module.exports = register;
